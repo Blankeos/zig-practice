1aceb01de9f706fce3fcd4a47f9ed3abb60d5cb1
Blankeos <carloantonioct@gmail.com>
2024-07-05 04:44:34 +0800

    docs: more on readme. I kind of gave up.

---
+++ 002-salary/src/main.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+
+pub fn main() !void {
+    std.debug.print("💵 Welcome to the Salary Estimator App!\n", .{});
+    std.debug.print("\n", .{});
+
+    std.debug.print("Enter your Currency: [USD | PHP]", .{});
+
+    var currency: i64 = ask_user();
+
+    std.debug.print("{s} currency", .{currency});
+
+    // // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    // // stdout is for the actual output of your application, for example if you
+    // // are implementing gzip, then only the compressed bytes should be sent to
+    // // stdout, not any debugging messages.
+    // const stdout_file = std.io.getStdOut().writer();
+    // var bw = std.io.bufferedWriter(stdout_file);
+    // const stdout = bw.writer();
+
+    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    // try bw.flush(); // don't forget to flush!
+}
+
+fn ask_user() !i64 {
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+
+    var buf: [10]u8 = undefined;
+
+    try stdout.print("A number please: ", .{});
+
+    if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |user_input| {
+        return std.fmt.parseInt(i64, user_input, 10);
+    } else {
+        return @as(i64, 0);
+    }
+}
+
+// IDK How to write tests :D
+// test "simple test" {
+//     var list = std.ArrayList(i32).init(std.testing.allocator);
+//     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+//     try list.append(42);
+//     try std.testing.expectEqual(@as(i32, 42), list.pop());
+// }
